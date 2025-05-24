008314e18d5b26777c56afba545beabf42214ec0
OcheOfAbuja <isaac.ochigbo@binghamuni.edu.eg>
2025-05-24 18:17:01 +0100

    Frontend: Hardcodes Google Client ID in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,13 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import CoinContextProvider from './context/CoinContext';
 import { AuthProvider } from './context/AuthContext'; 
 import { NavigationProvider } from './context/NavigationContext';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-if (!GOOGLE_CLIENT_ID) {
-  console.error("Google Client ID is not defined! Google login will not work.");
-  // You might want to throw an error or display a warning to the user
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    {GOOGLE_CLIENT_ID && (
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId="481025084065-272jm543jslcjd3ic6bnqb6gl1vmkt56.apps.googleusercontent.com">
       <CoinContextProvider>
         <NavigationProvider>
             <AuthProvider>
@@ -28,10 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           </NavigationProvider>
       </CoinContextProvider>
     </GoogleOAuthProvider>
-    )}
-    {!GOOGLE_CLIENT_ID && (
-      <div>Error: Google login not configured. Please set VITE_GOOGLE_CLIENT_ID.</div>
-    )}
     </BrowserRouter>
   </React.StrictMode>
 );
